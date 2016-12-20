b013ef4cd17b68086941e7f1be99a2e270748d6e
1016399594@qq.com <1016399594@qq.com>
2016-12-20 11:49:50 +0800

    11:49

--- index.php
+++ index.php
@@ -53,25 +53,37 @@ $signPackage = $jssdk->GetSignPackage();
 		        'openCard'
 		    ]
 		});
-		var latitude = 0,
-			longitude =0;
-		wx.getLocation({
-		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-		    success: function (res) {
-		        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-		        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-		        var speed = res.speed; // 速度，以米/每秒计
-		        var accuracy = res.accuracy; // 位置精度
-		    }
-		});
-		wx.openLocation({
-		    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-		    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-		    name: '', // 位置名
-		    address: '', // 地址详情说明
-		    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-		    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
-		});
+		  var latitude = 0,
+				longitude =0;
+		  function getLocation(){
+				wx.getLocation({
+				    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+				    success: function (res) {
+				        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+				        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+				        var speed = res.speed; // 速度，以米/每秒计
+				        var accuracy = res.accuracy; // 位置精度
+				        alert(latitude);
+				        alert(longitude);
+
+				    }
+				});
+		  }
+		
+		function openLocation(){
+			wx.openLocation({
+			    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+			    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+			    name: '', // 位置名
+			    address: '', // 地址详情说明
+			    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+			    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+			});
+		}
+		
+		document.getElementById("btn1").onclick=function(){
+			getLocation();
+		}
 	</script>
 	
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
@@ -93,6 +105,8 @@ $signPackage = $jssdk->GetSignPackage();
 		<div class="myOrder"></div>
 		<div class="integral"></div>
 		<div class="coupon"></div>
+		<input type="button" id="btn1">
+		<input type="button" id="btn2">
 	</div>
 	<div id="index_footer">
 		<figure>
