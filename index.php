8913ee53b47cda48c026ef4e7db6f5278e6bf8fc
1016399594@qq.com <1016399594@qq.com>
2016-12-20 10:11:37 +0800

    10:11

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ $signPackage = $jssdk->GetSignPackage();
 <head>
 	<meta charset="UTF-8">
 	<script type="text/javascript">
-		wx.config({
-    debug: true,
-    appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
-    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-    signature: '<?php echo $signPackage["signature"];?>',
-     jsApiList: [
+		  wx.config({
+		    debug: true,
+		    appId: '<?php echo $signPackage["appId"];?>',
+		    timestamp: <?php echo $signPackage["timestamp"];?>,
+		    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+		    signature: '<?php echo $signPackage["signature"];?>',
+		     jsApiList: [
         'checkJsApi',
         'onMenuShareWeibo',
         'onMenuShareQZone',
@@ -51,8 +51,17 @@ $signPackage = $jssdk->GetSignPackage();
         'addCard',
         'chooseCard',
         'openCard'
-      ]
-      });
+     ]
+		  });
+		  wx.getLocation({
+		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+		    success: function (res) {
+		        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+		        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+		        var speed = res.speed; // 速度，以米/每秒计
+		        var accuracy = res.accuracy; // 位置精度
+		    }
+		});
 	</script>
 	<script type="text/javascript" src ="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
